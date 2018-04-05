08d09bba5a25f1b3105f9cbc72bc67dfefcd9106
4edorov <4edorov@gmail.com>
2018-04-05 22:28:14 +0300

    glitch fixs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-monthsStr = 'January,February,March,April,May,June,July,August,September,October,November,December'
+const monthsStr = 'January,February,March,April,May,June,July,August,September,October,November,December'
 const months = monthsStr.split(',')
 const PORT = process.env.PORT || 3000
 
